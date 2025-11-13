6d0849a13cf2fecf94596654f9219075068e5655
tk-dao <tuankhanhdao1370@gmail.com>
2025-11-12 23:25:57 -0600

    Wire Tailwind/Daisy UI styling and entry setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthProvider.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>
 );
